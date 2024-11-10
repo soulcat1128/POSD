7b2c33fb8ed9ae20b38f585a742d3744c9836c54
soulcat1128 <eva38173991@gmail.com>
2024-11-10 23:52:09 +0800

    test 1110

---
+++ practice/Builder/demo/src/main/java/builder/pattern/Computer.java
@@ -0,0 +1,58 @@
+package builder.pattern;
+
+
+// using inner class with builder pattern
+public class Computer {
+    private String cpu;
+    private String ram;
+    private String storage;
+    private String gpu;
+
+    private Computer(Builder builder) {
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.storage = builder.storage;
+        this.gpu = builder.gpu;
+    }
+
+    public static class Builder {
+        private String cpu;
+        private String ram;
+        private String storage;
+        private String gpu;
+
+        public Builder cpu(String cpu) {
+            this.cpu = cpu;
+            return this;
+        }
+
+        public Builder ram(String ram) {
+            this.ram = ram;
+            return this;
+        }
+
+        public Builder storage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public Builder gpu(String gpu) {
+            this.gpu = gpu;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "cpu='" + cpu + '\'' +
+                ", ram='" + ram + '\'' +
+                ", storage='" + storage + '\'' +
+                ", gpu='" + gpu + '\'' +
+                '}';
+    }
+}
---
+++ practice/Builder/demo/src/test/Computer/ComputerTest.java
@@ -0,0 +1,19 @@
+package builder.pattern;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class ComputerTest {
+    // @Test
+    // public void test()
+    // {
+    //     assertEquals(1, 1, 0.1);
+    // }
+
+}
